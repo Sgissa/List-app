5bb5726b006a205e8715278588ce359d8a117514
Sgissa <143265121+Sgissa@users.noreply.github.com>
2023-10-29 19:57:15 -0400

    Add files via upload

---
+++ List-app/app/app.js
@@ -0,0 +1,518 @@
+
+var LISTS = [
+    {
+       "name":"Groceries",
+       "items":[
+          {
+             "name":"milk",
+             "checked":false,
+            
+          },
+          {
+             "name":"eggs",
+             "checked":false,
+            
+          },
+          {
+             "name":"banana",
+             "checked":false,
+            
+          },
+          {
+             "name":"Orange Juice",
+             "checked":false,
+            
+          },
+          {
+             "name":"onions",
+             "checked":false,
+           
+          },
+          {
+             "name":"tamato",
+             "checked":false,
+             
+          },
+          {
+             "name":"peppers",
+             "checked":false,
+             
+          },
+          {
+             "name":"oil",
+             "checked":false,
+             
+          },
+          {
+             "name":"apples",
+             "checked":false,
+             
+          },
+          {
+             "name":"chicken",
+             "checked":false,
+             
+          }
+       ]
+    },
+    {
+       "name":"Camping",
+       "items":[
+          {
+             "name":"Axe",
+             "checked":false
+          },
+          {
+             "name":"Cooler",
+             "checked":false
+          },
+          {
+             "name":"Fire Starter",
+             "checked":false
+          },
+          {
+             "name":"Med Aid",
+             "checked":false
+          },
+          {
+             "name":"Roasting Sticks",
+             "checked":false
+          },
+          {
+             "name":"Sleeping Bag",
+             "checked":false
+          },
+          {
+             "name":"Stove",
+             "checked":false
+          },
+          {
+             "name":"Tent",
+             "checked":false
+          },
+          {
+             "name":"Trash Bin",
+             "checked":false
+          },
+          {
+             "name":"Watter Bottle",
+             "checked":false
+          }
+       ]
+    },
+    {
+       "name":"Gym Equipment",
+       "items":[
+          {
+             "name":"Watter Bottle",
+             "checked":false
+          },
+          {
+             "name":"Wrist Straps",
+             "checked":false
+          },
+          {
+             "name":"Dumbbell",
+             "checked":false
+          },
+          {
+             "name":"Bench",
+             "checked":false
+          },
+          {
+             "name":"Treadmill",
+             "checked":false
+          },
+          {
+             "name":"Resistence Bands",
+             "checked":false
+          },
+          {
+             "name":"Kettlebell",
+             "checked":false
+          },
+          {
+             "name":"Yoga Mat",
+             "checked":false
+          },
+          {
+             "name":"Free Weights",
+             "checked":false
+          },
+          {
+             "name":"bicycle",
+             "checked":false
+          }
+       ]
+    },
+    {
+       "name":"Hair Products",
+       "items":[
+          {
+             "name":"Comb",
+             "checked":false
+          },
+          {
+             "name":"Shampoo",
+             "checked":false
+          },
+          {
+             "name":"Conditioner",
+             "checked":false
+          },
+          {
+             "name":"Blow Dryer",
+             "checked":false
+          },
+          {
+             "name":"Hair Ties",
+             "checked":false
+          },
+          {
+             "name":"Bonnet",
+             "checked":false
+          },
+          {
+             "name":"Clippers",
+             "checked":false
+          },
+          {
+             "name":"Trimmer",
+             "checked":false
+          },
+          {
+             "name":"razer",
+             "checked":false
+          },
+          {
+             "name":"Leave-IN Conditioner",
+             "checked":false
+          }
+       ]
+    },
+    {
+       "name":"Kitchen",
+       "items":[
+          {
+             "name":"Knife",
+             "checked":false
+          },
+          {
+             "name":"Fork",
+             "checked":false
+          },
+          {
+             "name":"Spoon",
+             "checked":false
+          },
+          {
+             "name":"Cutting Board",
+             "checked":false
+          },
+          {
+             "name":"Pan",
+             "checked":false
+          },
+          {
+             "name":"Pot",
+             "checked":false
+          },
+          {
+             "name":"Plates",
+             "checked":false
+          },
+          {
+             "name":"Cups",
+             "checked":false
+          },
+          {
+             "name":"Tupperware",
+             "checked":false
+          },
+          {
+             "name":"Microwave",
+             "checked":false
+          }
+       ]
+    },
+    {
+       "name":"Furniture",
+       "items":[
+          {
+             "name":"couch",
+             "checked":false
+          },
+          {
+             "name":"desk",
+             "checked":false
+          },
+          {
+             "name":"chair",
+             "checked":false
+          },
+          {
+             "name":"bed",
+             "checked":false
+          },
+          {
+             "name":"bedframe",
+             "checked":false
+          },
+          {
+             "name":"oven",
+             "checked":false
+          },
+          {
+             "name":"stove",
+             "checked":false
+          },
+          {
+             "name":"dishwasher",
+             "checked":false
+          },
+          {
+             "name":"washer",
+             "checked":false
+          },
+          {
+             "name":"Dryer",
+             "checked":false
+          }
+       ]
+    },
+    {
+       "name":"Gaming Setup",
+       "items":[
+          {
+             "name":"Desk",
+             "checked":false
+          },
+          {
+             "name":"Mic",
+             "checked":false
+          },
+          {
+             "name":"Boom arm",
+             "checked":false
+          },
+          {
+             "name":"Monitors",
+             "checked":false
+          },
+          {
+             "name":"PC",
+             "checked":false
+          },
+          {
+             "name":"Keyboard",
+             "checked":false
+          },
+          {
+             "name":"Mouse",
+             "checked":false
+          },
+          {
+             "name":"Speakers",
+             "checked":false
+          },
+          {
+             "name":"Power bank",
+             "checked":false
+          },
+          {
+             "name":"Headset",
+             "checked":false
+          }
+       ]
+    },
+    {
+       "name":"Wardrobe",
+       "items":[
+          {
+             "name":"Jacket",
+             "checked":false
+          },
+          {
+             "name":"Long-sleeve",
+             "checked":false
+          },
+          {
+             "name":"Shirt-sleeve",
+             "checked":false
+          },
+          {
+             "name":"Pants",
+             "checked":false
+          },
+          {
+             "name":"Shorts",
+             "checked":false
+          },
+          {
+             "name":"Shoes",
+             "checked":false
+          },
+          {
+             "name":"sweater",
+             "checked":false
+          },
+          {
+             "name":"Underwear",
+             "checked":false
+          },
+          {
+             "name":"Coat",
+             "checked":false
+          },
+          {
+             "name":"Socks",
+             "checked":false
+          }
+       ]
+    },
+    {
+       "name":"School Supplies",
+       "items":[
+          {
+             "name":"Backpack",
+             "checked":false
+          },
+          {
+             "name":"Mac",
+             "checked":false
+          },
+          {
+             "name":"pencil",
+             "checked":false
+          },
+          {
+             "name":"Pen",
+             "checked":false
+          },
+          {
+             "name":"Notebooks",
+             "checked":false
+          },
+          {
+             "name":"Books",
+             "checked":false
+          },
+          {
+             "name":"Ruler",
+             "checked":false
+          },
+          {
+             "name":"Pencil Sharpner",
+             "checked":false
+          },
+          {
+             "name":"eraser",
+             "checked":false
+          },
+          {
+             "name":"Sketch book",
+             "checked":false
+          }
+       ]
+    },
+    {
+       "name":"Office",
+       "items":[
+          {
+             "name":"Desk",
+             "checked":false
+          },
+          {
+             "name":"Office Chair",
+             "checked":false
+          },
+          {
+             "name":"Monitor",
+             "checked":false
+          },
+          {
+             "name":"Computer",
+             "checked":false
+          },
+          {
+             "name":"Desk Lamp",
+             "checked":false
+          },
+          {
+             "name":"Sticky Notes",
+             "checked":false
+          },
+          {
+             "name":"WhiteBoard",
+             "checked":false
+          },
+          {
+             "name":"File Cabinet",
+             "checked":false
+          },
+          {
+             "name":"Desk Organizer",
+             "checked":false
+          },
+          {
+             "name":"Printer",
+             "checked":false
+          }
+       ]
+    }
+ ];
+
+ function itemChecked(element, listIndex, itemIndex){
+    
+    $(element).parent().toggleClass("strike");
+    let checkedValue = !LISTS[listIndex].items[itemIndex].checked;
+    LISTS[listIndex].items[itemIndex].checked = checkedValue;
+    // console.log("checked ", LISTS);
+ }
+
+function addItem(listIndex){
+    let newItemName = $("#addItem").val();
+    let newItemObj = {
+        name: newItemName,
+        checked: false,
+    };
+    LISTS[listIndex].items.push(newItemObj);
+    loadListsItems(listIndex)
+}
+
+function deleteItem(listIndex, idx){
+   LISTS[listIndex].items.splice(idx,1);
+   loadListsItems(listIndex);
+}
+
+function loadListsItems(listIndex){
+    let listString = `<button class="backBtn" onclick="loadLists()">Back</button><ul>`
+    $.each(LISTS[listIndex].items,function(idx, item){
+        listString += `<li id="${idx}" class="${item.checked ? "strike" : ""}"><input ${item.checked ? (checked = "checked") : ""} type="checkbox" id="${idx}" name="${item.name}" onclick="itemChecked(this, ${listIndex}, ${idx})"><span>${item.name}</span>
+        <span class="delete" onclick="deleteItem(${listIndex}, ${idx})">delete</span></li>`;
+    });
+
+    listString += `</ul>
+    <div class="addItemInput">
+    <input id="addItem" type="text">
+    <button class="addBtn" onclick="addItem(${listIndex})">Add Item</button>
+    </div>`;
+    $("#app").html(listString);
+}
+
+
+function loadLists(){
+    let listString = "<ul>"
+    $.each(LISTS,function(idx, list){
+        listString += `<li id="${idx}" onclick="loadListsItems(${idx})">${list.name} <span class="right">Items: ${list.items.length}</span></li>`;
+    });
+
+    listString += "</ul>";
+    $("#app").html(listString);
+}
+
+function initListeners() {}
+
+$(document).ready(function(){
+    initListeners();
+});
